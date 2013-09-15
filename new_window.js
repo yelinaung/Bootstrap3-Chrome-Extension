08c121ffc033d1166870e782cedce765f174987b
mgpyone <mgpyone@mysteryzillion.com>
2013-09-15 11:21:38 +0630

    BS3 Offline!

--- new_window.js
+++ new_window.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function(tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('docs/index.html')}, function(tab) {
   // Tab opened.
   });
 });
